1d6d188ff2a4c7d04b41743b9d84e006af7e86fe
pascalkasichiza <pascalkasichiza@gmail.com>
2021-07-22 05:45:35 -0700

    ad image to assest file

--- lib/views/screens/welcome.dart
+++ lib/views/screens/welcome.dart
@@ -1,41 +1,55 @@
+import 'package:bonappetit/constants/constanst.dart';
+import 'package:bonappetit/utils/colors.dart';
 import 'package:bonappetit/views/widgets/widgets.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 class Welcome extends StatelessWidget {
   const Welcome({key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final size =MediaQuery.of(context).size;
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       body: Container(
-          height: size.height *.5,
-          width: size.width,
-          decoration: BoxDecoration(
-            color: Colors.red,
-            shape: BoxShape.circle,
+        height: size.height * .5,
+        width: size.width,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(Assets.roundPlat),
+            fit: BoxFit.fill,
           ),
-      child: BackgroundBlur(
-        child: Container(
-           child: Column(
-             children: [
-               RichText(
-                 text: TextSpan(
-                     text: 'Bonjour',
-                     style: TextStyle(
-                         color: Colors.black, fontSize: 18),
-                     children: <TextSpan>[
-                       TextSpan(text: ' Sign up',
-                           style: TextStyle(
-                               color: Colors.blueAccent, fontSize: 18),
-
-                       )
-                     ]
-                 ),
-               )
-             ],
-           ),),
-      ),
+          shape: BoxShape.circle,
+        ),
+        child: BackgroundBlur(
+          child: Container(
+            height: size.height,
+            child: Column(
+              children: [
+                RichText(
+                  text: TextSpan(
+                    text: "Bienvenue ",
+                    style: TextStyle(
+                        color: OrangeColor,
+                        fontWeight: FontWeight.bold,
+                        fontSize: FontSize.max,
+                        fontFamily: 'Butter'),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: "dans l'application",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      TextSpan(
+                        text: " de livraison de nourriture",
+                        style: TextStyle(fontSize: 18),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
