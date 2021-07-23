00884529a9ce358e0890a1d36ecf081b6786501e
pascalkasichiza <pascalkasichiza@gmail.com>
2021-07-23 16:06:28 -0700

    Create card promtion

--- lib/views/screens/homeIndex/homePage.dart
+++ lib/views/screens/homeIndex/homePage.dart
@@ -1,3 +1,5 @@
+import 'package:bonappetit/constants/constanst.dart';
+import 'package:bonappetit/utils/utils.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,6 +7,56 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final size = MediaQuery.of(context).size;
+    return Padding(
+      padding: const EdgeInsets.only(top: 80, left: 10, right: 10),
+      child: Column(
+        children: [
+          Container(
+            height: 160,
+            width: size.width,
+            decoration: BoxDecoration(
+              color: OrangeColor,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        "Goute a nos plats",
+                        style: TextStyle(
+                          color: WhiteColor,
+                          fontSize: FontSize.max_min,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      SizedBox(
+                        height: 50.0,
+                        width: 50.0,
+                        child: Placeholder(),
+                      )
+                    ],
+                  ),
+                  Text(
+                    "les plus populaire.",
+                    style: TextStyle(
+                      color: WhiteColor,
+                      fontSize: FontSize.max_meduim - 1,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
