74af58c903e3b9274c7ec8b9a6f4a8290319c837
pascalkasichiza <pascalkasichiza@gmail.com>
2021-07-23 16:45:20 -0700

    Creation partiel de l'interface

--- lib/views/screens/homeIndex/restaurantSPage.dart
+++ lib/views/screens/homeIndex/restaurantSPage.dart
@@ -1,12 +1,212 @@
+import 'package:bonappetit/constants/constanst.dart';
+import 'package:bonappetit/utils/utils.dart';
 import 'package:flutter/material.dart';
 
 class RestaurantPage extends StatelessWidget {
-  const RestaurantPage({ key }) : super(key: key);
+  const RestaurantPage({key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    final size = MediaQuery.of(context).size;
+    return Padding(
+      padding: const EdgeInsets.only(top: 80, left: 10, right: 10),
+      child: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 10),
+            Text(
+              "Top semaine",
+              style: TextStyle(
+                color: BlackColor,
+                fontSize: FontSize.meduim,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              child: Stack(
+                children: [
+                  Positioned(
+                    child: Container(
+                      height: 170,
+                      width: size.width,
+                      child: Image.asset(
+                        Assets.roundPlat,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    child: Container(
+                      height: 60,
+                      width: size.width,
+                      child: Container(
+                        color: GreyWithOpacity,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  RichText(
+                                    text: TextSpan(
+                                      text: "Chicken Pasta ",
+                                      style: TextStyle(
+                                        color: BlackColor,
+                                        fontSize: FontSize.lard,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      children: <TextSpan>[
+                                        TextSpan(
+                                          text: "by LaRosta",
+                                          style: TextStyle(
+                                            color: GreyColor,
+                                            fontSize: FontSize.meduim,
+                                            fontWeight: FontWeight.normal,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  SizedBox(height: 5),
+                                  Text(
+                                    "3k recommender cette semaine",
+                                    style: TextStyle(
+                                      color: GreyColor,
+                                      fontSize: FontSize.meduim - 2,
+                                      fontWeight: FontWeight.normal,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Text(
+                                    "14\$-120\$",
+                                    style: TextStyle(
+                                      color: GreyColor,
+                                      fontSize: FontSize.meduim - 2,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Text(
+                                    "4\$-12\$",
+                                    style: TextStyle(
+                                      color: BlackColor,
+                                      fontSize: FontSize.lard,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              child: Stack(
+                children: [
+                  Positioned(
+                    child: Container(
+                      height: 170,
+                      width: size.width,
+                      child: Image.asset(
+                        Assets.roundPlat,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    child: Container(
+                      height: 60,
+                      width: size.width,
+                      child: Container(
+                        color: GreyWithOpacity,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  RichText(
+                                    text: TextSpan(
+                                      text: "Chicken Pasta ",
+                                      style: TextStyle(
+                                        color: BlackColor,
+                                        fontSize: FontSize.lard,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      children: <TextSpan>[
+                                        TextSpan(
+                                          text: "by LaRosta",
+                                          style: TextStyle(
+                                            color: GreyColor,
+                                            fontSize: FontSize.meduim,
+                                            fontWeight: FontWeight.normal,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  SizedBox(height: 5),
+                                  Text(
+                                    "3k recommender cette semaine",
+                                    style: TextStyle(
+                                      color: GreyColor,
+                                      fontSize: FontSize.meduim - 2,
+                                      fontWeight: FontWeight.normal,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Text(
+                                    "14\$-120\$",
+                                    style: TextStyle(
+                                      color: GreyColor,
+                                      fontSize: FontSize.meduim - 2,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Text(
+                                    "4\$-12\$",
+                                    style: TextStyle(
+                                      color: BlackColor,
+                                      fontSize: FontSize.lard,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
