3d9de8828ff383099cda304c94d138c06f3b09fc
pascalkasichiza <pascalkasichiza@gmail.com>
2021-07-23 15:18:11 -0700

    create buttonNavigatorbar

--- lib/views/screens/homeBonAppetit.dart
+++ lib/views/screens/homeBonAppetit.dart
@@ -1,3 +1,5 @@
+import 'package:bonappetit/constants/constanst.dart';
+import 'package:bonappetit/utils/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:bonappetit/views/screens/homeIndex/homeIndex.dart';
 
@@ -10,12 +12,44 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int _selectedItem = 0;
-  var _pages = [];
+  var _pages = [
+    HomePage(),
+    RestaurantPage(),
+    CommandePage(),
+    ParametrePage(),
+    ProfilPage()
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: PageView(
+        children: _pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+              icon: Image.asset(Assets.home),
+              label: 'Home',
+              tooltip: 'Acceuil'),
+          BottomNavigationBarItem(
+              icon: Image.asset(Assets.restaurant),
+              label: 'Home',
+              tooltip: 'Restaurant'),
+          BottomNavigationBarItem(
+              icon: Image.asset(Assets.commande),
+              label: 'Home',
+              tooltip: 'Commande'),
+          BottomNavigationBarItem(
+              icon: Image.asset(Assets.parametre),
+              label: 'Home',
+              tooltip: 'Parametre'),
+          BottomNavigationBarItem(
+              icon: Image.asset(Assets.profil),
+              label: 'Home',
+              tooltip: 'Profil'),
+        ],
+      ),
     );
   }
 }
