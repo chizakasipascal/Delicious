097ded8b23d5343107dd7321af0d17022a8b38b7
pascalkasichiza <pascalkasichiza@gmail.com>
2021-07-23 16:58:32 -0700

    intrgration partiel

--- lib/views/screens/homeIndex/commandesPage.dart
+++ lib/views/screens/homeIndex/commandesPage.dart
@@ -1,3 +1,5 @@
+import 'package:bonappetit/utils/utils.dart';
+import 'package:bonappetit/views/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class CommandePage extends StatelessWidget {
@@ -5,6 +7,22 @@ class CommandePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final size = MediaQuery.of(context).size;
+    return Padding(
+      padding: const EdgeInsets.only(top: 80, left: 10, right: 10),
+      child: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            AnnonceCard(
+              size: size,
+              color: BluePenColor,
+            ),
+            SizedBox(height: 10),
+          ],
+        ),
+      ),
+    );
   }
 }
