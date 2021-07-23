eee0395d9b1207f50f283b63ec49222ac03e99d3
pascalkasichiza <pascalkasichiza@gmail.com>
2021-07-23 15:17:46 -0700

    add path for all icons

--- lib/constants/assets.dart
+++ lib/constants/assets.dart
@@ -1,7 +1,16 @@
 class Assets {
   static const String pathAssets = "assets/images/";
+  static const String pathIcons = "assets/images/icons";
   Assets._();
 
   static const String roundPlat = "${pathAssets}roundPlat.jpg";
   static const String burger = "${pathAssets}burger.png";
+
+  //Icons
+
+  static const String home = "${pathIcons}home.png";
+  static const String restaurant = "${pathIcons}restaurant.png";
+  static const String commande = "${pathIcons}commande.png";
+  static const String parametre = "${pathIcons}parametre.png";
+  static const String profil = "${pathIcons}profil.png";
 }
