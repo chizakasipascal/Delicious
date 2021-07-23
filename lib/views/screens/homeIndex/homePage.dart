54de838b456b3c25cf3111f5e8361cd7a7e1fef7
pascalkasichiza <pascalkasichiza@gmail.com>
2021-07-23 16:44:56 -0700

    Creation partiel de l'interface

--- lib/views/screens/homeIndex/homePage.dart
+++ lib/views/screens/homeIndex/homePage.dart
@@ -10,52 +10,246 @@ class HomePage extends StatelessWidget {
     final size = MediaQuery.of(context).size;
     return Padding(
       padding: const EdgeInsets.only(top: 80, left: 10, right: 10),
-      child: Column(
-        children: [
-          Container(
-            height: 160,
-            width: size.width,
-            decoration: BoxDecoration(
-              color: OrangeColor,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
+      child: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 170,
+              width: size.width,
+              child: Card(
+                color: OrangeColor,
+                elevation: 2,
+                shadowColor: GreyWithOpacity,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
+                      Row(
+                        children: [
+                          Text(
+                            "Goute a nos plats",
+                            style: TextStyle(
+                              color: WhiteColor,
+                              fontSize: FontSize.max_min,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(width: 20),
+                          SizedBox(
+                            height: 50.0,
+                            width: 50.0,
+                            child: Image.asset(Assets.commande),
+                          )
+                        ],
+                      ),
                       Text(
-                        "Goute a nos plats",
+                        "les plus populaire.",
                         style: TextStyle(
                           color: WhiteColor,
-                          fontSize: FontSize.max_min,
+                          fontSize: FontSize.max_meduim - 2,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
-                      SizedBox(width: 20),
-                      SizedBox(
-                        height: 50.0,
-                        width: 50.0,
-                        child: Image.asset(Assets.commande),
-                      )
                     ],
                   ),
-                  Text(
-                    "les plus populaire.",
-                    style: TextStyle(
-                      color: WhiteColor,
-                      fontSize: FontSize.max_meduim - 1,
-                      fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Top semaine",
+              style: TextStyle(
+                color: BlackColor,
+                fontSize: FontSize.meduim,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              child: Stack(
+                children: [
+                  Positioned(
+                    child: Container(
+                      height: 170,
+                      width: size.width,
+                      child: Image.asset(
+                        Assets.roundPlat,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    child: Container(
+                      height: 60,
+                      width: size.width,
+                      child: Container(
+                        color: GreyWithOpacity,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  RichText(
+                                    text: TextSpan(
+                                      text: "Chicken Pasta ",
+                                      style: TextStyle(
+                                        color: BlackColor,
+                                        fontSize: FontSize.lard,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      children: <TextSpan>[
+                                        TextSpan(
+                                          text: "by LaRosta",
+                                          style: TextStyle(
+                                            color: GreyColor,
+                                            fontSize: FontSize.meduim,
+                                            fontWeight: FontWeight.normal,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  SizedBox(height: 5),
+                                  Text(
+                                    "3k recommender cette semaine",
+                                    style: TextStyle(
+                                      color: GreyColor,
+                                      fontSize: FontSize.meduim - 2,
+                                      fontWeight: FontWeight.normal,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Text(
+                                    "14\$-120\$",
+                                    style: TextStyle(
+                                      color: GreyColor,
+                                      fontSize: FontSize.meduim - 2,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Text(
+                                    "4\$-12\$",
+                                    style: TextStyle(
+                                      color: BlackColor,
+                                      fontSize: FontSize.lard,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
                     ),
                   ),
                 ],
               ),
             ),
-          ),
-        ],
+            SizedBox(height: 10),
+            Container(
+              child: Stack(
+                children: [
+                  Positioned(
+                    child: Container(
+                      height: 170,
+                      width: size.width,
+                      child: Image.asset(
+                        Assets.roundPlat,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    child: Container(
+                      height: 60,
+                      width: size.width,
+                      child: Container(
+                        color: GreyWithOpacity,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  RichText(
+                                    text: TextSpan(
+                                      text: "Chicken Pasta ",
+                                      style: TextStyle(
+                                        color: BlackColor,
+                                        fontSize: FontSize.lard,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      children: <TextSpan>[
+                                        TextSpan(
+                                          text: "by LaRosta",
+                                          style: TextStyle(
+                                            color: GreyColor,
+                                            fontSize: FontSize.meduim,
+                                            fontWeight: FontWeight.normal,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  SizedBox(height: 5),
+                                  Text(
+                                    "3k recommender cette semaine",
+                                    style: TextStyle(
+                                      color: GreyColor,
+                                      fontSize: FontSize.meduim - 2,
+                                      fontWeight: FontWeight.normal,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Text(
+                                    "14\$-120\$",
+                                    style: TextStyle(
+                                      color: GreyColor,
+                                      fontSize: FontSize.meduim - 2,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Text(
+                                    "4\$-12\$",
+                                    style: TextStyle(
+                                      color: BlackColor,
+                                      fontSize: FontSize.lard,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
