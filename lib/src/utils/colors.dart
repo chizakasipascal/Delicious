6ea1ab6201864ad1d38024aedf4a7711aa327c42
chizakasipascal <pascalkasichiza@gmail.com>
2022-04-30 17:49:42 +0200

    Changer color for app

--- lib/src/utils/colors.dart
+++ lib/src/utils/colors.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 const kBlackColor = Colors.black;
 const kGreyColor = Color(0xff525A6C);
-const KPrimary = Color.fromARGB(255, 253, 84, 38);
+const KPrimary = Colors.deepOrange; //Color.fromARGB(255, 253, 84, 38);
 const kWhiteGreyColor = Color.fromARGB(255, 222, 220, 220);
